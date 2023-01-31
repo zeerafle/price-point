92e5cf8c7cbbf78fd752746042ff674150734b5e
Sam Fareez <vsefareez@gmail.com>
2023-01-31 22:49:08 +0800

    update tokopedia module to use selenium and include merchant name

--- price_point/routes/price_scraping/modules/tokopedia.py
+++ price_point/routes/price_scraping/modules/tokopedia.py
@@ -1,28 +1,35 @@
 import datetime
-from bs4 import BeautifulSoup
-import requests
+import os
+import pytz
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 def tokopedia(link):
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/109.0'}
-    webpage = requests.get(link, headers=headers)
-    dom = BeautifulSoup(webpage.content, 'html.parser')
-    product_name = dom.find('h1').text
-    price = dom.find('p', {'data-testid': 'pdpProductPrice'}).text
+    driver = webdriver.Firefox()
+    driver.get(url)
+    product_name = driver.find_element(By.TAG_NAME, 'h1').text
+    merchant_anchor = driver.find_element(By.CSS_SELECTOR, 'a[data-testid="llbPDPFooterShopName"]')
+    merchant_name = merchant_anchor.find_element(By.TAG_NAME, 'h2').text
+    price = driver.find_element(By.CSS_SELECTOR, 'p[data-testid="pdpProductPrice"]').text
     price = price[2:].replace('.', '')
     price = int(price)
-    last_updated = datetime.datetime.now()
+    last_updated = datetime.datetime.now(pytz.timezone('Asia/Makassar')).strftime('%Y-%m-%dT%H:%M:%S.%f%z')
 
     return {
         'name': product_name,
+        'merchant': merchant_name,
         'price': price,
         'last_updated': last_updated
     }
 
 
 if __name__ == '__main__':
-    from price_point.config import SCRAPE_DO_API_KEY
+    from config import SCRAPE_DO_API_KEY, DRIVER_PATH
 
+    os.environ['PATH'] += DRIVER_PATH
+    os.environ['MOZ_HEADLESS'] = "1"
     url = 'https://www.tokopedia.com/gojeteindonesia/jete-x-mouse-gaming-msx1-rgb-wired-6-programmable-buttons-original'
     scrape_url = f'http://api.scrape.do?token={SCRAPE_DO_API_KEY}&url={url}'
-    print(tokopedia(scrape_url))
+    print(tokopedia(url))
